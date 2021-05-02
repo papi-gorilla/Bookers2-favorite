ad2e6f35dbc0e58fa1f0c9433f159324c94c2a59
papi-gorilla <jindeensadayoshi@gmail.com>
2021-05-02 09:53:51 +0000

    [Add] いいね機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  has_many :favorites ,dependent: :destroy
 
   validates :name, uniqueness: true
   validates :name, length: {in: 2..20}
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   root to: "homes#top"
   get "home/about" => "homes#about"
-  resources :books, only:[:index, :create, :show, :edit, :update, :destroy]
+  resources :books, only:[:index, :create, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users, only:[:index, :show, :edit, :update]
 end
